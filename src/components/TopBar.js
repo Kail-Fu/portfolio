7fa45aac84e2adbe9fb5094c1c1a930071579cdf
coolraven222 <kailiang_fu+cs1300@brown.edu>
2022-12-03 01:32:51 -0500

    vscode framework

---
+++ src/components/TopBar.js
@@ -0,0 +1,38 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.css';
+import "../styles/Bar.css"
+import vslogo from '../images/vscode.png';
+import * as React from 'react';
+import { green, red, yellow } from '@mui/material/colors';
+import CircleIcon from '@mui/icons-material/Circle';
+
+
+function TopBar() {
+    return (
+        <Navbar bg="dark" sticky="top">
+            <Nav className="me-auto">
+                <Nav.Item style={{ leftMargin: "20px" }}> <img
+                    src={vslogo}
+                    width="22"
+                    height="22"
+                    className="d-inline-block align-top"
+                    alt="VS Code logo"
+                /></Nav.Item>
+                <Nav.Item>File</Nav.Item>
+                <Nav.Item>Edit</Nav.Item>
+                <Nav.Item>View</Nav.Item>
+                <Nav.Item>Go</Nav.Item>
+                <Nav.Item>Run</Nav.Item>
+                <Nav.Item>Terminal</Nav.Item>
+                <Nav.Item>Help</Nav.Item>
+                <Nav.Item style={{ marginLeft: '300px' }}>Cool Raven - Visual Studio Code</Nav.Item>
+            </Nav>
+            <CircleIcon className='circleicon' sx={{ color: yellow[800], fontSize: 20 }} />
+            <CircleIcon className='circleicon' sx={{ color: green[500], fontSize: 20 }} />
+            <CircleIcon className='circleicon' sx={{ color: red[500], fontSize: 20 }} />
+        </Navbar >
+    );
+}
+
+export default TopBar;
